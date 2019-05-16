7df00eb7670f384e93b900befd7c9c8a85a33b6c
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2019-05-16 22:29:18 +0200

    Improve fish config.

--- linux/configs/fish.sh
+++ linux/configs/fish.sh
@@ -6,5 +6,13 @@ sudo apt-get update
 sudo apt-get install fish -y
 
 echo "Configuring fish to be the default shell ..."
-chsh -s /usr/local/bin/fish
-echo /usr/local/bin/fish | sudo tee -a /etc/shells
+chsh -s $(which fish)
+sudo usermod -s $(which fish) $(whoami)
+
+if grep -Fxq $(which fish) /etc/shells 
+then 
+  # fish has already been added to shells
+else
+  # add fish to shells
+  echo $(which fish) | sudo tee -a /etc/shells
+fi
