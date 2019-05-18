59130ac973d545dda272978a89af5161ae57e464
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2019-05-18 23:48:19 +0200

    Disable single-click in files.

--- linux/configs/elementary.sh
+++ linux/configs/elementary.sh
@@ -2,8 +2,9 @@
 
 echo "Configuring basic stuff in elementary os ..."
 
-# show hidden files
+# configure files
 gsettings set org.pantheon.files.preferences show-hiddenfiles true
+gsettings set io.elementary.files.preferences single-click false
 
 # add minimize button
 gsettings set org.gnome.desktop.wm.preferences action-right-click-titlebar 'minimize'
