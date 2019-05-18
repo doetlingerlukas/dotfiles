65d5a119aa2efc9cc01b7949bf7bb966d7df5481
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2019-05-18 13:19:59 +0200

    Add `config` mode to linux dotfiles.

--- linux/main.sh
+++ linux/main.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-echo "Updating system ..."
-sudo apt update
-sudo apt upgrade -y
+while [[ $# -gt 0 ]]
+do
+  key="$1"
+  case $key in
+    -m|--mode)
+      MODE="$2"
+      shift
+      shift
+      ;;
+  esac
+done
 
 execute_configs () {
   for file in ./configs/*.sh; do
@@ -10,4 +18,17 @@ execute_configs () {
   done 
 }
 
-execute_configs
+if [ "${MODE}" == "config" ]
+then
+  echo -e "\e[30;103mStarting dotfiles in configuration mode ...\e[0m"
+  execute_configs
+else 
+  echo -e "\e[30;42mStarting dotfiles in setup mode ...\e[0m"
+
+  echo "Updating system ..."
+  sudo apt update
+  sudo apt upgrade -y
+
+  bash ./installs.sh -H
+  execute_configs
+fi
