382dcca74e6f262dfc47504d45bc0ac13fd9f73c
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2019-05-19 12:08:52 +0200

    WIP on dock config.

--- linux/configs/dock.sh
+++ linux/configs/dock.sh
@@ -2,5 +2,19 @@
 
 echo "Configuring dock ..."
 
-sudo apt install dconf-tools -y
+# delete all default items
+rm -f ~/.config/plank/dock1/launchers/*.dockitem
 
+header="[PlankDockItemPreferences]"
+
+dock_items="
+  org.pantheon.files,
+  org.terminal."
+
+for dock_item in ${dock_items//,/ }
+do
+  cat > ${dock_item}.dockitem << EOF
+    ${header}
+    file//${dock_item}
+EOF
+done
