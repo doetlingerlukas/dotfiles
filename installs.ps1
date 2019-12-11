274b1192def81ced19f7a2a30ede29efdcea71f6
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2019-12-11 20:06:40 +0100

    Update choco packages.

--- installs.ps1
+++ installs.ps1
@@ -1,4 +1,12 @@
-# Add package provider
+function installChocoPackages {
+  Param([String[]]$packages)
+
+  foreach ($p in $packages) {
+    choco install $p -y
+  }
+}
+
+# Add powershell-package provider
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 
 # Install YAML module
@@ -10,6 +18,12 @@ $content = ''
 foreach ($line in $fileContent) { $content = $content + "`n" + $line }
 $choco = ConvertFrom-YAML $content
 
-foreach ($program in $choco.common) {
-  Write-Host $program
+# install common programs
+installChocoPackages($choco.common)
+
+# install system specific programs
+if ($env:COMPUTERNAME.ToLower().contains("razer")) {
+  installChocoPackages($choco.laptop)
+} else {
+  installChocoPackages($choco.desktop)
 }
