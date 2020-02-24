8a45006333fecbe1fcda097cfc074bf9f696dd7d
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2020-02-24 22:01:26 +0100

    Simplify laptop.rb

--- lib/laptop.rb
+++ lib/laptop.rb
@@ -2,11 +2,12 @@
 
 require 'open3'
 require 'os'
+require 'command'
 
 def laptop?
   if OS.windows?
-    stdout, _, status = Open3.capture3("powershell", "-command", "Write-Host $env:computername")
-    stdout.downcase.include?('razer')
+    out, _, status = ps_command('Write-Host $env:computername')
+    out.downcase.include?('razer')
   else
     ENV['HOSTNAME'].downcase.include?('razer')
   end
