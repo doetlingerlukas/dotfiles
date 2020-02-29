289de4a142c3c1b22d24bb4d69241e3f91b6bfcc
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2020-02-29 10:39:48 +0100

    Remove unused scripts.

--- linux/configs/yarn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Installing and configuring yarn ..."
-
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-
-sudo apt update
-sudo apt install --no-install-recommends yarn -y
