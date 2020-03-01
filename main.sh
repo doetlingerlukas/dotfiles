85e2bc655add1da464ff46bd45e42ec89b5d1d3a
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2020-03-01 17:18:59 +0100

    Update azure-pipelines.yaml

--- main.sh
+++ main.sh
@@ -23,7 +23,7 @@ verify_brew_installation () {
   if ! type "brew" &> /dev/null
   then
     sudo apt install build-essential curl file git -y
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+    echo | sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
     test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
     test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
     test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
