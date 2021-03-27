6516e899c5ebf6c34ff892036105b90a9610fcd0
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2021-03-27 22:13:53 +0100

    Update `pwsh` config.

--- res/configs/Microsoft.PowerShell_profile.ps1
+++ res/configs/Microsoft.PowerShell_profile.ps1
@@ -1,41 +1,27 @@
+# Set default user for oh-my-posh
+$env:POSH_SESSION_DEFAULT_USER = $env:UserName
+
 Import-Module Get-ChildItemColor
-Import-Module -Name z
+Import-Module z
 Import-Module PSWriteColor
+Import-Module posh-git
+Import-Module oh-my-posh
 
 # Set l and ls alias to use the new Get-ChildItemColor cmdlets
 Set-Alias l Get-ChildItemColor -Option AllScope
 Set-Alias ls Get-ChildItemColorFormatWide -Option AllScope
 
-# Set default user for oh-my-posh theme
-$DefaultUser = $env:UserName
+# Remove 'diff' alias
+Remove-Alias diff -Force
+
+New-Alias which get-command
 
 # Helper function to change directory to development workspace
-Function cdw { Set-Location "C:\Users\$DefaultUser\Documents\Git" }
+Function cdw { Set-Location "$env:USERPROFILE\Documents\Git" }
 
 # Helper function to set location to the User Profile directory
 Function cuserprofile { Set-Location ~ }
 Set-Alias ~ cuserprofile -Option AllScope
-
-# Remove 'diff' alias
-Remove-Alias diff -Force
-
-Function createFile {
-  param ([string]$file)
-
-  if ($file -eq $null) {
-    throw "No filename supplied"
-  }
-
-  if (Test-Path $file) {
-    (Get-ChildItem $file).LastWriteTime = Get-Date
-  }
-  else {
-    Write-Output $null > $file
-  }
-}
-  
-Set-Alias touch createFile
-New-Alias which get-command
 
 # helper function to show unicode character
 Function U {
@@ -52,8 +38,5 @@ Function U {
   throw "Invalid character code $Code"
 }
 
-Import-Module -Name posh-git
-Import-Module -Name oh-my-posh
-
-# default the prompt to agnoster oh-my-posh theme
-Set-Theme agnoster
+# default the prompt to Powerline oh-my-posh theme
+Set-PoshPrompt -Theme Powerline
