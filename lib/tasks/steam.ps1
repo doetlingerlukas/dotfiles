f3b4ce3c9d4d65ea4de8a5ca94848ae997e596a7
Lukas Dötlinger <lukas@doetlinger.at>
2022-11-28 14:41:35 +0100

    Rewrite steam task.

--- lib/tasks/steam.ps1
+++ lib/tasks/steam.ps1
@@ -10,5 +10,24 @@ Task steam {
   # Don't start Steam with Windows.
   Remove-RegValue -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' -Name 'Steam'
 
+  # Download required PowerShell module.
+  $remote_uri = 'https://raw.githubusercontent.com/doetlingerlukas/Steam-GetOnTop/master/Modules/SteamTools/SteamTools.psm1'
+  $steam_module_path = "$env:USERPROFILE\Documents\PowerShell\Modules\SteamTools.psm1"
+  Invoke-WebRequest -Uri $remote_uri -OutFile $steam_module_path
+
+  Import-Module -DisableNameChecking $steam_module_path
+
+  $config = 'C:\Program Files (x86)\Steam\userdata\136275020\config\localconfig.vdf'
+  $config_object = ConvertFrom-VDF -InputObject (Get-Content $config)
+
+  # Remember state of Steam Friends window.
+  $config_object.UserLocalConfigStore | Add-Member -MemberType NoteProperty -Name 'StartupState.Friends' -Value 1
+
+  # Disable Steam news pop-up.
+  $news_object = [PSCustomObject]@{NotifyAvailableGames = 0}
+  $config_object.UserLocalConfigStore | Add-Member -MemberType NoteProperty -Name 'News' -Value $news_object
+
+  ConvertTo-VDF -InputObject $config_object | Out-File $config
+
   Write-Host 'Successfully configured Steam client!'
 }
