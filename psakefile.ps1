e3ce3f3d75350a046111ba8cbde4826dd74acb3d
Lukas Dötlinger <lukas@doetlinger.at>
2022-11-28 15:45:47 +0100

    Remove old tasks.

--- psakefile.ps1
+++ psakefile.ps1
@@ -5,14 +5,6 @@ Get-ChildItem -Filter '*.ps1' '.\lib\tasks\' | ForEach-Object {
 
 Task default -Depends setup
 
-Task Init {
+Task setup -Depends uninstalls, installs, configs {}
 
-}
-
-Task setup -Depends uninstalls, installs, configs {
-  Write-Host "Test"
-}
-
-Task configs -Depends csgo, explorer, git, latex, nvm, pwsh, ssh, terminal, vscode, windows {
-
-}
+Task configs -Depends csgo, explorer, git, latex, nvm, pwsh, ssh, steam, terminal, vscode, windows {}
