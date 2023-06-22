5b4b5c02e9d93b333b739e8cb0869c18195adf52
Lukas Dötlinger <lukas@doetlinger.at>
2023-06-22 15:31:43 +0200

    Fix python setup and install `pyenv` with `pip`.

--- lib/tasks/python.ps1
+++ lib/tasks/python.ps1
@@ -1,11 +1,27 @@
+Import-Module -DisableNameChecking $PSScriptRoot\..\"system-info.psm1"
+
 Task python {
+  Assert-ElevatedPrivileges
+
   Write-Host "Setting up python environment ..."
 
-  Exec { python } "Error, python is not available."
+  Remove-Item -Force "$env:UserProfile\AppData\Local\Microsoft\WindowsApps\python*"
+
+  Exec { regedit /s "$env:UserProfile\scoop\apps\python\current\install-pep-514.reg" }
+
+  Exec { python --version } "Error, python is not available."
 
   pip install --upgrade pip
   pip install pipenv
   pip install notebook
+  pip install pyenv-win --target "$env:UserProfile\\.pyenv"
+
+  [System.Environment]::SetEnvironmentVariable('PYENV', "$env:UserProfile\.pyenv\pyenv-win\", 'User')
+  [System.Environment]::SetEnvironmentVariable('PYENV_ROOT', "$env:UserProfile\.pyenv\pyenv-win\", 'User')
+  [System.Environment]::SetEnvironmentVariable('PYENV_HOME', "$env:UserProfile\.pyenv\pyenv-win\", 'User')
+
+  $new_path = "$env:UserProfile\.pyenv\pyenv-win\bin;" + "$env:UserProfile\.pyenv\pyenv-win\shims;" + [System.Environment]::GetEnvironmentVariable('path', 'User')
+  [System.Environment]::SetEnvironmentVariable('path', $new_path, 'User')
 
   Write-Host "Successfully set up python environment!"
 }
